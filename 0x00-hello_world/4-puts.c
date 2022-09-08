bd29af65adfb42e6fc862c2f83f748b5a5ba7dbb
Samuel Nwodo <samset911@gmail.com>
2022-09-08 13:50:42 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main - main block
- * Return: 0
+ * main - prints the phase "programming is like a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
-int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
