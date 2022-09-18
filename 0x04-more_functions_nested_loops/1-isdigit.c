1bd2832b5f0019b660bcf44097a5d247eb4458a7
Samuel Nwodo <samset911@gmail.com>
2022-09-18 12:39:01 -0700

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - check
+ *
+ * Return: Always 0
+ */
+
+int _isdigit(int c)
+{
+	if (num >= 0; && num <= 9)
+		return (1);
+	else
+		return (0);
+}
