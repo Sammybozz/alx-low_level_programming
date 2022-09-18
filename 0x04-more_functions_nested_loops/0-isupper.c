7429e654ba74efa4276960dbb6f2797f5b55f4ac
Samuel Nwodo <samset911@gmail.com>
2022-09-18 12:20:54 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "maim.h"
+
+/**
+ * _isupper - check
+ *
+ *
+ */
+
+int _isupper(int c)
+{
+	if (upper >= 65 && upper <= 90)
+	{
+		return (i);
+	}
+	else
+	{
+		return (0);
+	}
+
+	return (0)
+}
